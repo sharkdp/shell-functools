8e81397a4b7225863d5b582df28bd0b178dbff8f
sharkdp <davidpeter@web.de>
2018-04-01 19:37:20 +0200

    Fix PEP8 warnings

--- ft/ft/commands/sort_by_key.py
+++ ft/ft/commands/sort_by_key.py
@@ -1,21 +1,20 @@
 from ft.types import T_ARRAY
 from ft.command import Command
 
+
 class SortByKey(Command):
     def __init__(self):
         super().__init__("sort_by_key")
         self.arr = []
-        
 
     def handle_input(self, value):
         val = value
         if value.fttype == T_ARRAY and self.column is not None:
             val = value.value[self.column - 1]
-        
+
         result = self.function(val)
-        self.arr.append( (val, result) )
-    
+        self.arr.append((val, result))
+
     def finalize(self):
         arr = sorted(self.arr, key=lambda x: x[1])
         list(map(lambda x: self.print_formatted(x[0]), arr))
-        
