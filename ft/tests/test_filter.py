ab2f8e7aa5b6355c4d7ced62b29ac03d7bc10f4c
sharkdp <davidpeter@web.de>
2018-04-01 21:40:38 +0200

    Add 'take_while' command, closes #20

--- ft/tests/test_filter.py
+++ ft/tests/test_filter.py
@@ -1,19 +1,20 @@
 from mock_command import mock_command
 from ft.commands.filter import Filter
 
-test_filter = mock_command(Filter)
-
 
 def test_filter_const_false():
+    test_filter = mock_command(Filter)
     test_filter.set_input("const", ["false"], ["3", "5"])
     test_filter.assert_output([])
 
 
 def test_filter_const_true():
+    test_filter = mock_command(Filter)
     test_filter.set_input("const", ["true"], ["3", "5"])
     test_filter.assert_output(["3", "5"])
 
 
 def test_filter_greater_equals():
+    test_filter = mock_command(Filter)
     test_filter.set_input("greater_equals", ["3"], ["3", "2", "5", "4", "1"])
     test_filter.assert_output(["3", "5", "4"])
