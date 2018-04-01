85f5ae3ac0cefd8132ad9a92b34dfc842195330f
sharkdp <davidpeter@web.de>
2018-04-01 19:49:31 +0200

    Fix test_filter module

--- ft/tests/test_filter.py
+++ ft/tests/test_filter.py
@@ -1,19 +1,19 @@
 from mock_command import mock_command
 from ft.commands.filter import Filter
 
-map_filter = mock_command(Filter)
+test_filter = mock_command(Filter)
 
 
 def test_filter_const_false():
-    map_filter.set_input("const", ["false"], ["3", "5"])
-    map_filter.assert_output([])
+    test_filter.set_input("const", ["false"], ["3", "5"])
+    test_filter.assert_output([])
 
 
 def test_filter_const_true():
-    map_filter.set_input("const", ["true"], ["3", "5"])
-    map_filter.assert_output(["3", "5"])
+    test_filter.set_input("const", ["true"], ["3", "5"])
+    test_filter.assert_output(["3", "5"])
 
 
 def test_filter_greater_equals():
-    map_filter.set_input("greater_equals", ["3"], ["3", "2", "5", "4", "1"])
-    map_filter.assert_output(["3", "5", "4"])
+    test_filter.set_input("greater_equals", ["3"], ["3", "2", "5", "4", "1"])
+    test_filter.assert_output(["3", "5", "4"])
