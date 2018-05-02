9848ead448f36e966a75ae15e2858be24768dbb6
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-05-02 22:07:00 +0200

    Only change parser in filter

--- ft/ft/commands/filter.py
+++ ft/ft/commands/filter.py
@@ -3,11 +3,34 @@
 from ft.types import T_BOOL, T_ARRAY
 from ft.error import panic
 from ft.command import Command
+from ft.functions import function_list
 
 class Filter(Command):
     def __init__(self, name="filter"):
         super().__init__(name)
         self.exit_when_false = False
+
+
+    def add_command_arguments(self, parser):
+        parser.add_argument('--negate', '-n',
+                            action='store_true',
+                            help='negate predicate')
+        return parser
+
+    def parse_args(self):
+        parser = self.get_argument_parser()
+
+        args = parser.parse_args()
+
+        self.column = args.column
+        self.arguments = args.args
+        self.negate_predicate = args.negate
+
+        function_name = args.function
+        try:
+            self.function = function_list[function_name]
+        except KeyError:
+            panic("Function not found: '{}'".format(function_name))
 
     def handle_input(self, value):
         val_to_test = value
