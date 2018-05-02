a436ead436a0b0d008b4c6435a915ee4c83196d2
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-05-02 15:37:55 +0200

    Add --negate flag to filter

--- ft/ft/commands/filter.py
+++ ft/ft/commands/filter.py
@@ -4,20 +4,24 @@ from ft.types import T_BOOL, T_ARRAY
 from ft.error import panic
 from ft.command import Command
 
-
 class Filter(Command):
     def __init__(self, name="filter"):
         super().__init__(name)
         self.exit_when_false = False
 
     def handle_input(self, value):
+
         val_to_test = value
         if self.column and value.fttype == T_ARRAY:
             val_to_test = value.value[self.column - 1]
 
         result = self.function(val_to_test)
 
+        if self.negate_predicate:
+            result.value = not result.value
+
         if result.fttype == T_BOOL:
+
             if result.value:
                 self.print_formatted(value)
             else:
